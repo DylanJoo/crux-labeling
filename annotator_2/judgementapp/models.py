e249061839531b2248019435a3cd8f3d5cfc409a
Dylan <dylanjootw@gmail.com>
2025-05-06 19:32:50 +0200

    split into two directories

---
+++ annotator_2/judgementapp/models.py
@@ -0,0 +1,135 @@
+import json
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Document(models.Model):
+    docId = models.CharField(max_length=100)
+    # add document
+
+    def __str__(self):
+        return self.docId
+
+    def get_content(self):
+        content = ""
+        try:
+            with open(settings.DATA_DIR+"/"+self.docId) as f:
+                read = f.read()
+                try:
+                    data = json.loads(read)
+                    content = json.loads(read)['contents']
+                except:
+                    content += read
+        except Exception:
+            content = "Could not read file %s" % settings.DATA_DIR+"/"+self.docId
+        return content
+
+def default_query_types():
+    return {str(i): 0 for i in range(3)}
+def default_query_metadata():
+    return {'num_oracle_passages': -1, 'type': 'none'}
+
+def default_query_questions():
+    return {str(i): None for i in range(15)}
+def default_query_question_labels():
+    return {str(i): -1 for i in range(15)}
+def default_query_nuggets():
+    return {str(i): '' for i in range(50)}
+
+class Query(models.Model):
+    # qId = models.IntegerField()
+    qId = models.CharField(max_length=100)
+    text = models.CharField(max_length=300, default="NA")
+    topic = models.JSONField(default={'1': 1})
+    # crux
+    metadata = models.JSONField(default=default_query_metadata)
+    questions = models.JSONField(default=default_query_questions)
+    question_labels = models.JSONField(default=default_query_question_labels)
+    reference = models.TextField()
+    report = models.TextField()
+
+    # labeling
+    nuggets = models.JSONField(default=default_query_nuggets)
+
+    def __str__(self):
+        answerabiliy = [self.question_labels[k] for k in self.questions]
+        questions = [v if v is not None else "NA" for k, v in self.questions.items()]
+        nuggets = [v for k, v in self.nuggets.items()]
+
+        data_dict = {
+                "id": self.qId,
+                "question_based_nugget": {q: n for (q, n) in zip(questions, nuggets)},
+                "answerability": answerabiliy,
+                "coverage": sum(a==1 for a in answerabiliy) / len(answerabiliy),
+        }
+        to_return = json.dumps(data_dict)
+        return to_return + '\n'
+
+    def unfinished(self):
+        n_total = sum([1 for v in self.question_labels.values() if v != -2])
+        n_judged = sum([1 for v in self.question_labels.values() if v >= 0])
+
+        if n_judged == 0:
+            return 0 
+        elif n_total != n_judged:
+            return 1
+        else:
+            return 2
+
+    def unclassified(self):
+        return sum([int(v) for v in self.type.values()]) == 0
+
+    def num_unjudged_docs(self):
+        unjugded = [1 for judgement in self.judgements() if max(judgement.relevances.values()) == -1]
+        return len(unjugded)
+
+    def num_judgements(self):
+        return len(self.judgements())
+
+    def judgements(self):
+        return Judgement.objects.filter(query=self.id)
+
+def default_judgement_relevances():
+    return {str(i): -1 for i in range(15)}
+def default_judgement_rationales():
+    return {str(i): "" for i in range(15)}
+
+class Judgement(models.Model):
+    labels = {-1: 'Unjudged', 0: 'Not relvant', 1: 'minimally relevant', 2:'limited relevant', 3:'partially relevant', 4:'mostly relevant', 5:'fully relevant'}
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    comment = models.TextField(default="", null=True)
+    # relevance = models.IntegerField()
+
+    relevances = models.JSONField(default=default_judgement_relevances)
+    rationales = models.JSONField(default=default_judgement_rationales)
+    judged = models.BooleanField(default=False)
+
+    def __str__(self):
+        data_dict = {
+                "id": self.query.qId,
+                "document": self.document.docId,
+                "relevances": self.relevances,
+        }
+        to_return = json.dumps(data_dict)
+        return to_return + '\n'
+
+    def update_judged(self):
+        self.judged = (min(self.relevances.values()) != -1)
+
+    def label(self):
+        return "{" + ", ".join(f"Q-{i}: {r}" for i, r in enumerate(self.relevances.values()) if r != -1) + "}"
+
+# old document-level judgement
+# class Judgement(models.Model):
+#     labels = {-1: 'Unjudged', 0: 'Not relvant', 1: 'minimally relevant', 2:'limited relevant', 3:'partially relevant', 4:'mostly relevant', 5:'fully relevant'}
+#     query = models.ForeignKey(Query, on_delete=models.CASCADE)
+#     document = models.ForeignKey(Document, on_delete=models.CASCADE)
+#     comment = models.TextField(default="", null=True)
+#     relevance = models.IntegerField()
+#
+#     def __str__(self):
+#         return '%s Q0 %s %s\n' % (self.query.qId, self.document.docId, self.relevance)
+#
+#     def label(self):
+#         return self.labels[self.relevance]
---
+++ annotator_2/judgementapp/views.py
@@ -0,0 +1,276 @@
+# Create your views here.
+from io import StringIO
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import Context, loader, RequestContext
+# from django.core.servers.basehttp import FileWrapper
+from django.shortcuts import render, get_object_or_404
+from wsgiref.util import FileWrapper
+
+from judgementapp.models import *
+import json
+
+def examples(request, qId=None, docId=None):
+    context = {'back_path': request.path.replace('examples/', '')}
+    context.update({'is_doc': 'doc' in request.path})
+    return render(request, 'judgementapp/examples.html', context)
+
+def index(request):
+    queries = Query.objects.order_by('qId')
+    output = ', '.join([q.text for q in queries])
+
+    # template = loader.get_template('judgementapp/index.html')
+    context = {'queries': queries}
+    # return HttpResponse(template.render(request, context))
+    return render(request, 'judgementapp/index.html', context)
+
+def qrels(request):
+    judgements = Judgement.objects.exclude(relevance=-1)
+
+    response = HttpResponse(judgements, content_type='application/force-download')
+    # response['Content-Disposition'] = 'attachment; filename=qrels.txt'
+    response['Content-Disposition'] = 'attachment; filename=qrels.jsonl'
+    return response
+
+def qlabels(request):
+    # queries = Query.objects.all()
+    queries = Query.objects.exclude(text="NA")
+
+    response = HttpResponse(queries, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=qlabels.jsonl'
+    #response['X-Sendfile'] = myfile
+    # It's usually a good idea to set the 'Content-Length' header too.
+    # You can also set any other required headers: Cache-Control, etc.
+    return response
+
+def query_list(request):
+    # see if we need to add the filter (about the judged)
+    queries = Query.objects.order_by('id')
+    return render(request, 'judgementapp/query_list.html', {'queries': queries})
+
+def query(request, qId):
+    query = Query.objects.get(qId=qId)
+    judgements = Judgement.objects.filter(query=query.id)
+
+    if "clear" in request.POST:
+        for n, question in query.questions.items():
+            query.nuggets[n] = ""
+        for n, label in query.question_labels.items():
+            query.question_labels[n] = 0
+
+    else:
+        if "csrfmiddlewaretoken" in request.POST:
+            for n, question in query.questions.items():
+                query.nuggets[n] = request.POST.getlist(f'nugget-{n}')[0]
+            for n, label in query.question_labels.items():
+                try:
+                    query.question_labels[n] = int(request.POST.getlist(f'qlabel-{n}')[0])
+                except:
+                    query.question_labels[n] = -2
+
+    query.save()
+    query.length = len(query.text)
+
+    # navigation
+    prev = None
+    try:
+        prev = Query.objects.get(id=query.id-1)
+    except:
+        pass
+
+    next = None
+    try:
+        next = Query.objects.get(id=query.id+1)
+    except:
+        pass
+
+    return render(request, 'judgementapp/query.html', 
+            {'query': query, 'judgements': judgements, 
+             'prev': prev, 'next': next}
+    )
+
+def document(request, qId, docId):
+    document = Document.objects.get(docId=docId)
+    query = Query.objects.get(qId=qId)
+
+    judgements = Judgement.objects.filter(query=query.id)
+    judgement = Judgement.objects.filter(query=query.id, document=document.id)[0]
+    rank = -1
+    for (count, j) in enumerate(judgements):
+        if j.id == judgement.id:
+            rank = count+1
+            break
+
+
+    prev = None
+    try:
+        prev = Judgement.objects.filter(query=query.id).get(id=judgement.id-1)
+    except:
+        pass
+
+    next_query = None
+    next = None
+    try:
+        next = Judgement.objects.filter(query=query.id).get(id=judgement.id+1)
+    except:
+        try:
+            next_query = Query.objects.get(id=query.id+1)
+        except:
+            pass
+
+    content = document.get_content()
+
+    return render(request, 'judgementapp/document.html', 
+            {'document': document, 'query': query, 
+                'judgement': judgement, 'next': next, 'prev': prev, 
+                'rank': rank, 'total_rank': judgements.count(), 
+                'next_query': next_query,
+                'content': content})
+
+def judge(request, qId, docId):
+    query = get_object_or_404(Query, qId=qId)
+    document = get_object_or_404(Document, docId=docId)
+    # relevance = request.POST['relevance']
+
+    judgements = Judgement.objects.filter(query=query.id)
+    judgement, created = Judgement.objects.get_or_create(query=query.id, document=document.id)
+    # judgement.relevance = int(relevance)
+
+    judged = [(s.split('-')[-1], request.POST.getlist(s)[0]) for s in request.POST if s.startswith('relevance-')]
+    for n, rel in judged:
+        judgement.relevances[n] = int(rel)
+
+    judged = [(s.split('-')[-1], request.POST.getlist(s)[0]) for s in request.POST if s.startswith('rationale-')]
+    for n, rationale in judged:
+        judgement.rationales[n] = rationale
+
+    judgement.update_judged()
+    judgement.save()
+
+    next = None
+    try:
+        next = Judgement.objects.filter(query=query.id).get(id=judgement.id+1)
+        if 'next' in request.POST:
+            document = next.document
+            judgement = next
+            next = Judgement.objects.filter(query=query.id).get(id=judgement.id+1)
+    except:
+        pass
+
+    prev = None
+    try:
+        prev = Judgement.objects.filter(query=query.id).get(id=judgement.id-1)
+    except:
+        pass
+
+    rank = -1
+    for (count, j) in enumerate(judgements):
+        if j.id == judgement.id:
+            rank = count+1
+            break
+
+
+    content = document.get_content()
+
+    # return render(request, 'judgementapp/upload.html', context)
+    return render(request, 'judgementapp/document.html', 
+            {'document': document, 'query': query, 
+                'judgement': judgement, 'next': next, 'prev': prev, 
+                'rank': rank, 'total_rank': judgements.count(), 
+                'content': content
+            }) 
+
+
+def reset(request):
+    # remove queries
+    queries = Query.objects.all()
+    n = len(queries)
+    queries.delete()
+
+    return render(request, 'judgementapp/upload.html', {
+        "deleted": False, "amount": n
+    })
+
+def delete(request):
+    # remove results
+    judgements = Judgement.objects.filter(judged=False)
+    n = len(judgements)
+    judgements.delete()
+
+    return render(request, 'judgementapp/upload.html', {
+        "deleted": True, "amount": n
+    })
+
+def upload(request):
+    context = {}
+
+    if 'queryFile' in request.FILES:
+        f = request.FILES['queryFile']
+        qryCount = 0
+        for i, query in enumerate(f):
+            data = json.loads(query)
+            qid = data['qid']
+            text = data['topic']
+            metadata = {
+                'num_oracle_passages': data['type'][1],
+                'type': data['type'][0],
+            }
+            questions = data['questions']
+            reference = data['report']
+            report = data['response']
+            query, created = Query.objects.get_or_create(qId=qid)
+
+            if created:
+                query.text = text
+                query.metadata = metadata
+                for i, question in enumerate(questions):
+                    query.questions[i] = question
+                    if question is None:
+                        query.question_labels[i] = -2
+                query.reference = reference
+                query.report = report
+                query.save()
+                qryCount += 1
+
+        context['uploaded'] = True
+        context['queries'] = qryCount
+        return render(request, 'judgementapp/upload.html', context)
+
+    if 'resultsFile' in request.FILES:
+        f = request.FILES['resultsFile']
+        qryCount, docCount, judCount = 0, 0, 0
+        for result in f:
+            qid, z, docid, rank, score, desc = result.decode().strip().split()
+
+            # check query
+            query, created = Query.objects.get_or_create(qId=qid)
+            if created:
+                query.text = "NA"
+                query.save()
+                qryCount += 1
+
+            # check document
+            document, created = Document.objects.get_or_create(docId=docid)
+            if created:
+                document.text = "NA"
+                document.save()
+                docCount += 1
+
+            # check judgement
+            judgement = Judgement.objects.filter(
+                    query=query.id, document=document.id
+            )
+            if len(judgement) == 0:
+                judgement = Judgement()
+                judgement.query = query
+                judgement.document = document
+                judgement.relevance = -1
+                judgement.save()
+                judCount += 1
+                
+        context['uploaded'] = True
+        context['documents'] = docCount
+        context['judgements'] = judCount
+        context['invalid_queries'] = qryCount
+
+    return render(request, 'judgementapp/upload.html', context)
